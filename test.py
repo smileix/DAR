c5f78f4316a444b1599c16306262b96d9480550b
Smile <1149393438@qq.com>
2021-07-03 21:49:10 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,5 @@
-# 模型预测
-y.predicted = model.predict(x_test)
-accuracy_rate = accuracy(y_test, y_predicted, True, output_num)
-print("acc", accuracy_rate)
-y.predicted = np.reshape(y_predicted, [-1, output_num])
-y.test = np.reshape(y_test, [-1, output_num])
-predict_label = np.argmax(y_predicted, axis=1)
-y_test_label = np.argmax(y_test, axis=1)
-cm_plot(y_test_label, predict_label).show()
-print(confusion_matrix(y_test_label, predict_label))
-return accuracy_rate
+from DAR import baseline_model_text
+
+x_train, y_train, x_test, y_test, word_index = text_prepare_newWithPadding()
+acc = baseline_model_text(x_train, y_train, x_test, y_test, word_index)
+print(acc)
